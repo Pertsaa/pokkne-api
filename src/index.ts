68fbab993241658b3af9fc9c603fc0c70c2d3c3d
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2022-02-01 16:04:59 +0200

    Connect database with typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server';
+import { createConnection } from 'typeorm';
 
 import { OpinionResolver } from './resolvers/OpinionResolver';
 
 const app = async () => {
+  await createConnection({
+    type: 'postgres',
+    host: 'db',
+    username: 'docker',
+    password: 'secret',
+    database: 'docker',
+    synchronize: true,
+    connectTimeoutMS: 10000,
+    entities: ['build/entities/*.js'],
+  });
+
   const schema = await buildSchema({
     resolvers: [OpinionResolver],
   });
